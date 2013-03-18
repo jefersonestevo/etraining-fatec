725eb545bbe3cc83bc743eaa958cb63f339a3c2d
Jeferson Estevo <jefersonestevo@gmail.com>
2013-03-18 17:48:33 -0300

    Inserção do Primefaces. Criação do template. Criação das constraint de segurança da aplicação.

---
+++ etraining-web/src/br/com/etraining/web/managedbeans/EtrainingManagedBean.java
@@ -0,0 +1,16 @@
+package br.com.etraining.web.managedbeans;
+
+public abstract class EtrainingManagedBean {
+
+	public void addErrorMessage(String message) {
+		// TODO - Implementar
+	}
+
+	public void addInfoMessage(String message) {
+		// TODO - Implementar
+	}
+
+	public void addSuccessMessage(String message) {
+		// TODO - Implementar
+	}
+}
