1965cd7413ab496f83dfe6edf971dd5a85f95173
Jeferson Estevo <jefersonestevo@gmail.com>
2013-04-02 10:23:49 -0300

    Implementação do permissionamento de acordo com o perfil de acesso do usuário.

---
+++ etraining-web/src/br/com/etraining/web/security/SecurityChecker.java
@@ -0,0 +1,20 @@
+package br.com.etraining.web.security;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import br.com.etraining.web.managedbeans.EtrainingManagedBean;
+
+@Named
+@SessionScoped
+public class SecurityChecker extends EtrainingManagedBean {
+
+	private static final long serialVersionUID = -3460371068901136271L;
+
+	public boolean temPermissao(String role) {
+		return FacesContext.getCurrentInstance().getExternalContext()
+				.isUserInRole(role);
+	}
+
+}
