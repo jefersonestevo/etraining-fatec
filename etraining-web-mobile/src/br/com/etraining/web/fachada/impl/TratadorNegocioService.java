5ecc6f6427fe7125c8e93694e9cbec8befd84200
Jeferson Estevo <jefersonestevo@gmail.com>
2013-05-07 00:05:57 -0300

    Criação do projeto etraining-web-mobile.

---
+++ etraining-web-mobile/src/br/com/etraining/web/fachada/impl/TratadorNegocioService.java
@@ -0,0 +1,44 @@
+package br.com.etraining.web.fachada.impl;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.etraining.client.fachada.ejb.IEtrainingService;
+import br.com.etraining.client.vo.interfaces.IVO;
+import br.com.etraining.client.vo.transporte.CodigoExcecao;
+import br.com.etraining.client.vo.transporte.impl.VORequest;
+import br.com.etraining.client.vo.transporte.impl.VOResponse;
+import br.com.etraining.web.exceptions.ViewException;
+
+@Named("tratadorNegocioMobile")
+public class TratadorNegocioService implements Serializable {
+
+	private static final long serialVersionUID = -4838224680755413276L;
+
+	@Inject
+	private IEtrainingService serviceDelegate;
+
+	public IVO executa(IVO request) throws ViewException {
+		VORequest req = new VORequest();
+		req.setRequest(request);
+		VOResponse resp = serviceDelegate.executa(req);
+
+		if (resp.getCodigoErro() != null) {
+			throw new ViewException(CodigoExcecao.getCodigoExcecao(resp
+					.getCodigoErro()));
+		}
+
+		return resp.getResponse();
+	}
+
+	public IEtrainingService getServiceDelegate() {
+		return serviceDelegate;
+	}
+
+	public void setServiceDelegate(IEtrainingService serviceDelegate) {
+		this.serviceDelegate = serviceDelegate;
+	}
+
+}
---
+++ etraining-web-mobile/src/br/com/etraining/web/managedbean/EtrainingManagedBean.java
@@ -0,0 +1,86 @@
+package br.com.etraining.web.managedbean;
+
+
+import java.io.Serializable;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import br.com.etraining.web.exceptions.ViewException;
+
+public abstract class EtrainingManagedBean implements Serializable {
+
+	private static final long serialVersionUID = 640707863239671690L;
+
+	public void addErrorMessage(String titulo, String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, titulo, message));
+	}
+
+	public void addErrorMessage(String titulo) {
+		addErrorMessage(titulo, null);
+	}
+
+	public void addInfoMessage(String titulo, String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, message));
+	}
+
+	public void addInfoMessage(String titulo) {
+		addInfoMessage(titulo, null);
+	}
+
+	public void addWarnMessage(String titulo, String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, titulo, message));
+	}
+
+	public void addWarnMessage(String titulo) {
+		addWarnMessage(titulo, null);
+	}
+
+	public void showSuccessMessage() {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO,
+						getMessage("Message_Sucesso"), null));
+	}
+
+	public void addExceptionMessage(ViewException e) {
+		addErrorMessage(getMessage("messageException", e.getCodigoExcecao()
+				.getCodigo().toString()));
+	}
+
+	public String getMessage(String chave) {
+		return getMessage("message", chave);
+	}
+
+	private String getMessage(String resource, String chave) {
+		try {
+			ResourceBundle bundle = FacesContext
+					.getCurrentInstance()
+					.getApplication()
+					.getResourceBundle(FacesContext.getCurrentInstance(),
+							resource);
+			return bundle.getString(chave);
+		} catch (MissingResourceException e) {
+			return chave;
+		}
+	}
+
+	public Long getLongParameter(String param) {
+		return new Long(getStringParameter(param));
+	}
+
+	public String getStringParameter(String param) {
+		return FacesContext.getCurrentInstance().getExternalContext()
+				.getRequestParameterMap().get(param);
+	}
+
+	protected void limparForm() {
+		FacesContext.getCurrentInstance().getViewRoot().findComponent("main")
+				.getChildren().clear();
+	}
+}
